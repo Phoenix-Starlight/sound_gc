e95cee620fb6146ae78f2128c0a6b3c1d4e8a520
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-14 16:44:21 +0800

    Fix modid

--- common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
+++ common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public final class SoundGC {
-    public static final String MOD_ID = "sound_gc";
+    public static final String MOD_ID = "soundgc";
 
     public final static Logger LOGGER = LogManager.getLogger("SoundGC");
 
