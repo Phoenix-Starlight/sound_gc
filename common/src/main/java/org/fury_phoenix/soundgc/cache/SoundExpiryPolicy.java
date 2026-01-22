4706eabb99d1b3dc38796967dca04c076deae9ba
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-22 17:50:17 +0800

    Relocate loggers and put markers for filtering

--- common/src/main/java/org/fury_phoenix/soundgc/cache/SoundExpiryPolicy.java
+++ common/src/main/java/org/fury_phoenix/soundgc/cache/SoundExpiryPolicy.java
@@ -5,10 +5,13 @@ import com.github.benmanes.caffeine.cache.RemovalCause;
 import com.mojang.blaze3d.audio.SoundBuffer;
 import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.fury_phoenix.soundgc.SoundGC;
 import org.fury_phoenix.soundgc.mixin.ChannelBufferUnbinder;
 import org.fury_phoenix.soundgc.mixin.OpenAlErrorChecker;
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +21,12 @@ import java.util.concurrent.TimeUnit;
 public class SoundExpiryPolicy implements Expiry<ResourceLocation, SoundBuffer> {
 
     private final static long SOUND_RETENTION_DURATION = 120;
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private final static Marker EVICTION = MarkerManager.getMarker("EVICTION");
+
+    private final static Marker TIMESTAMP = MarkerManager.getMarker("TIMESTAMP");
 
     public interface InjectableChannelAccess {
         void soundgc$setChannelAccess(ChannelAccess channelAccess);
@@ -39,14 +48,14 @@ public class SoundExpiryPolicy implements Expiry<ResourceLocation, SoundBuffer> 
         OpenAlErrorChecker.sound_gc$checkALError("Flush errors");
         var handle = channelAccess.createHandle(Library.Pool.STATIC).join();
         handle.execute(c -> ((ChannelBufferUnbinder) c).soundgc$unbindUsedBuffers());
-        LOGGER.debug("Evicting {}", k);
+        LOGGER.debug(EVICTION, "Evicting {}", k);
         buf.discardAlBuffer();
     }
 
     @Override
     public long expireAfterCreate(@NonNull ResourceLocation key, @NonNull SoundBuffer value, long currentTime) {
         long expiration = ((SoundBufferDuration) value).sound_gc$duration() + TimeUnit.SECONDS.toNanos(SOUND_RETENTION_DURATION);
-        SoundGC.LOGGER.debug("Resource Location: {}, Current time: {}, Expiration time: {}", key, currentTime, expiration);
+        LOGGER.debug(TIMESTAMP, "Resource Location: {}, Current time: {}, Expiration time: {}", key, currentTime, expiration);
         return expiration;
     }
 
