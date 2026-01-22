54be11222d92eefee9e04f7c8d324394f7ec7d39
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-22 17:43:59 +0800

    Remove unused init code

--- common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
+++ common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
@@ -5,10 +5,4 @@ import org.apache.logging.log4j.Logger;
 
 public final class SoundGC {
     public static final String MOD_ID = "soundgc";
-
-    public final static Logger LOGGER = LogManager.getLogger("SoundGC");
-
-    public static void init() {
-        // Write common init code here.
-    }
 }
--- forge/src/main/java/org/fury_phoenix/soundgc/forge/ExampleModForge.java
+++ forge/src/main/java/org/fury_phoenix/soundgc/forge/ExampleModForge.java
@@ -1,6 +1,5 @@
 package org.fury_phoenix.soundgc.forge;
 
-import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,10 +8,5 @@ import org.fury_phoenix.soundgc.SoundGC;
 @Mod(SoundGC.MOD_ID)
 public final class ExampleModForge {
     public ExampleModForge() {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(SoundGC.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
-        // Run our common setup.
-        SoundGC.init();
     }
 }
