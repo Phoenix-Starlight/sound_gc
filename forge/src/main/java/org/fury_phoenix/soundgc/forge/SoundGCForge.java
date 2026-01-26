4498d0cd917c03e3f3e0749fa310ef9f33affe24
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-26 15:00:21 +0800

    Rename obligatory forge class

--- forge/src/main/java/org/fury_phoenix/soundgc/forge/SoundGCForge.java
+++ forge/src/main/java/org/fury_phoenix/soundgc/forge/SoundGCForge.java
@@ -1,12 +1,8 @@
 package org.fury_phoenix.soundgc.forge;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.fury_phoenix.soundgc.SoundGC;
 
 @Mod(SoundGC.MOD_ID)
-public final class ExampleModForge {
-    public ExampleModForge() {
-    }
-}
+public final class SoundGCForge {}
