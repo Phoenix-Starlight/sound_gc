35f7977d49dcf50e404eb3a1c8b5e95a84931e57
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-26 20:07:06 +0800

    Remove unused imports

--- common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
+++ common/src/main/java/org/fury_phoenix/soundgc/SoundGC.java
@@ -1,8 +1,5 @@
 package org.fury_phoenix.soundgc;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public final class SoundGC {
     public static final String MOD_ID = "soundgc";
 }
