8f76cff3f9d41ae6cddfe9a2aa4c9ccacd32fabb
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2026-01-29 19:54:30 +0800

    Update forge maven URL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
         maven("https://maven.architectury.dev/")
-        maven("https://files.minecraftforge.net/maven/")
+        maven("https://maven.minecraftforge.net/")
     }
 }
 
